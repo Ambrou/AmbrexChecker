4bba1f5d4479d636e5f24ef642a6e6f6b0e7a8d2
ambrou <aucun>
2014-12-13 16:48:13 +0100

    Rajout du nouveau scénario sur la configuration

---
+++ AmbrexCheckerBDD/ConfigurationDeLApplicationSteps.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace AmbrexCheckerBDD
+{
+    [Binding]
+    public class ConfigurationDeLApplicationSteps
+    {
+        [Given(@"l'application lancé avec une ligne de commande")]
+        public void SoitLApplicationLanceAvecUneLigneDeCommande()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"la ligne de commande contient '-amont=""(.*)""")]
+        public void SoitLaLigneDeCommandeContient_Amont(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"la ligne de commande contient '-aval=""(.*)""")]
+        public void SoitLaLigneDeCommandeContient_Aval(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"la ligne de commande ne contient pas de commutateur -amont")]
+        public void SoitLaLigneDeCommandeNeContientPasDeCommutateur_Amont()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"la ligne de commande ne contient pas de commutateur -aval")]
+        public void SoitLaLigneDeCommandeNeContientPasDeCommutateur_Aval()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"j'analyse la ligne de commande")]
+        public void QuandJAnalyseLaLigneDeCommande()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"le fichier contenant les exigences amonts est fichier amont\.txt")]
+        public void AlorsLeFichierContenantLesExigencesAmontsEstFichierAmont_Txt()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"le fichier contenant les exigences avals est fichier aval\.doc")]
+        public void AlorsLeFichierContenantLesExigencesAvalsEstFichierAval_Doc()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"un message d'(.*)'est pas renseigné""")]
+        public void AlorsUnMessageDEstPasRenseigne(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"l'application arrête son traitement")]
+        public void AlorsLApplicationArreteSonTraitement()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ AmbrexCheckerBDD/Features/ConfigurationDeLApplication.feature
@@ -0,0 +1,28 @@
+﻿Fonctionnalité: Configuration de l'application
+  En tant qu' utilisateur d'Agex
+  Je veux configurer mon système
+  Afin de définir les fichiers à analyser
+
+Scénario: Configuration réussi du plugin
+  Soit l'application lancé avec une ligne de commande
+    Et la ligne de commande contient '-amont="fichier amont.txt"
+    Et la ligne de commande contient '-aval="fichier aval.doc"
+  Quand j'analyse la ligne de commande
+  Alors le fichier contenant les exigences amonts est fichier amont.txt
+    Et le fichier contenant les exigences avals est fichier aval.doc
+
+Scénario: Configuration du plugin sans fichier amont
+  Soit l'application lancé avec une ligne de commande
+    Et la ligne de commande contient '-aval="fichier aval.doc"
+    Et la ligne de commande ne contient pas de commutateur -amont
+  Quand j'analyse la ligne de commande
+  Alors un message d'erreur apparait "le fichier amont n'est pas renseigné"
+    Et l'application arrête son traitement
+
+Scénario: Configuration du plugin sans fichier aval
+  Soit l'application lancé avec une ligne de commande
+    Et la ligne de commande contient '-amont="fichier amont.doc"
+    Et la ligne de commande ne contient pas de commutateur -aval
+  Quand j'analyse la ligne de commande
+  Alors un message d'erreur apparait "le fichier aval n'est pas renseigné"
+    Et l'application arrête son traitement
